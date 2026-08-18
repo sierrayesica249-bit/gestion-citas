5977597e7b28e47e19ec39c26cd4c66c78d1a6bf
yesica sierra <sierrayesica249@gmail.com>
2026-08-18 11:34:29 -0500

    Cambiar logo por logotipo SENA naranja y actualizar favicon

--- src/shared/components/SenaLogo.jsx
+++ src/shared/components/SenaLogo.jsx
@@ -1,7 +1,7 @@
 export default function SenaLogo({ size = 36, className = "" }) {
   return (
     <img
-      src="/SENA.jpg"
+      src="/SENA.png"
       alt="Logo SENA"
       className={className}
       style={{ width: size, height: size, objectFit: "contain", maxWidth: "100%" }}
